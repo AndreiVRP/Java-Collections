5d0e36b75abcba7a9bf857a78097609fba0ed22a
AndreiVRP <andrei.hrabovich.vrp@gmail.com>
2018-11-21 11:40:08 +0300

    ArrayList vs LinkedList

--- src/ArrayListTest.java
+++ src/ArrayListTest.java
@@ -5,7 +5,7 @@ public class ArrayListTest {
     public static void main(String[] args) {
 
         //initializing a dynamic array of integers
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        List<Integer> arrayList = new ArrayList<>();
 
         //populating the array with numbers
         for (int i = 1; i < 12; i++) {
@@ -32,5 +32,17 @@ public class ArrayListTest {
         //removing specific elements
         arrayList.remove(10); //if you addressed index 11, the system would throw an exception (the array contains only 10 elements, which is why index 11 is out of bounds).
         System.out.println(arrayList);
+
+        //by default, the underlying array is 10 elements long, but we can extend it by setting the initial capacity if you can estimate the approximate amount of elements the array will eventually contain.
+        //this is a good practice designed to avoid recreating the underlying array ech time a new element is created beyond the initial capacity.
+        List<Integer> numbers = new ArrayList<>(100);
+
+        for (int i = 1; i < 101; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+
     }
 }
---
+++ src/LinkedListTest.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class LinkedListTest {
+    public static void main(String[] agr) {
+        List<Integer> linkedList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+//        linkedList.add(23);
+//        linkedList.add(99);
+//        System.out.println(linkedList.size());
+//        System.out.println(linkedList.get(0));
+
+        measurePopulateTime(linkedList);
+        measurePopulateTime(arrayList);
+        linkedList.clear();
+        arrayList.clear();
+        measurePopulateTimeWithIndex(linkedList);
+        measurePopulateTimeWithIndex(arrayList);
+        measureGetTime(linkedList);
+        measureGetTime(arrayList);
+
+
+        /*
+        USE CASES:
+        adding to the end of the list or reading data form the list -> use ArrayList (default choice)
+        removing from the list or adding to the beginning of the list (or adding/removing quite a lot) -> use LinkedList
+        */
+    }
+
+    public static void measurePopulateTime(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            list.add(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("The time spent on populating - " + list.getClass() + " = " + (end - start));
+        System.out.println(list.size());
+    }
+
+    public static void measurePopulateTimeWithIndex(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            list.add(0,i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("The time spent on populating with index - " + list.getClass() + " = " + (end - start));
+        System.out.println(list.size());
+    }
+
+    public static void measureGetTime(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            list.get(i);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("The time spent on getting elements from - " + list.getClass() + " = " + (end - start));
+        System.out.println(list.size());
+    }
+
+
+}
+
